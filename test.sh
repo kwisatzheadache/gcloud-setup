3c30eee5d6bd998b3882a60316ee93faa42ff0b4
Michael <michaelhorvath87@gmail.com>
2017-07-13 00:09:44 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 apt-get install git
-pip install https://github.com/kwisatzheadache/fast-style-clone
+git clone https://github.com/kwisatzheadache/fast-style-clone
 apt-get install python-pip python-dev
 pip download tensorflow
 pip install --U -user tensorflow*.whl
