5091c4c8849a672a731987e5d06b3cc9b09f653a
Michael <michaelhorvath87@gmail.com>
2017-07-13 00:41:45 -0500

    Update fast-style.sh

--- fast-style.sh
+++ fast-style.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/kwisatzheadache/fast-style-clone
 cd fast-style-clone
+mkdir output
+mkdir output/tests
+mkdir output/checkpoint
 apt-get install python-pip python-dev
 pip download tensorflow
 pip install --U -user tensorflow*.whl
