008431e5ce8202bdaf382a17896119f19b483dc0
kwisatzheadache <michaelhorvath87@gmail.com>
2017-07-13 15:15:38 -0500

    add style files

---
+++ fast-style-setup.sh
@@ -0,0 +1,10 @@
+apt-get install git
+pip install https://github.com/kwisatzheadache/fast-style-clone
+apt-get install python-pip python-dev
+pip download tensorflow
+pip install --U -user tensorflow*.whl
+pip install scipy pillow
+
+setup.sh
+
+echo 'setup complete'
---
+++ tensorflow-install.sh
@@ -0,0 +1,5 @@
+apt-get install python-dev
+apt-get install python-pip
+pip download tensorflow
+pip install --u -U tensorflow*.whl
+
