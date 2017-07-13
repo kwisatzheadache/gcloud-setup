a5e5627386da66d1182cb09c22614e2c6700414f
Michael <michaelhorvath87@gmail.com>
2017-07-13 00:33:19 -0500

    Update fast-style.sh

--- fast-style.sh
+++ fast-style.sh
@@ -1,9 +1,10 @@
 git clone https://github.com/kwisatzheadache/fast-style-clone
+cd fast-style-clone
 apt-get install python-pip python-dev
 pip download tensorflow
 pip install --U -user tensorflow*.whl
 pip install scipy pillow
 
-setup.sh
+./setup.sh
 
 echo 'setup complete'
