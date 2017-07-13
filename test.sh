a96a73461126ead17cba934415aa1871a01eb5bf
kwisatzheadache <michaelhorvath87@gmail.com>
2017-07-12 19:26:41 -0500

    test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+apt-get install git
+pip install https://github.com/kwisatzheadache/fast-style-clone
+apt-get install python-pip python-dev
+pip download tensorflow
+pip install --U -user tensorflow*.whl
+pip install scipy pillow
+
+setup.sh
